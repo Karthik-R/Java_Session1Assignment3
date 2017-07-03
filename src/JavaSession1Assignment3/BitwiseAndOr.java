b61ec09b3586ceda02931fb7b9a1358c93a8f61f
karth <karth@Karthik-HP>
2017-07-03 23:37:33 +0530

    Version 2

--- src/JavaSession1Assignment3/BitwiseAndOr.java
+++ src/JavaSession1Assignment3/BitwiseAndOr.java
@@ -2,6 +2,12 @@ package JavaSession1Assignment3;
 
 import java.util.Scanner;
 
+// The program is to print the truth table of Bitwise AND and Bitwise OR
+/* The question contains to print truth table of Bitwise AND and Logical AND, 
+ * but Logical AND is of boolean type and it's input cannot be taken from user
+ * and logical AND can be used only in conditional statements where it can be either TRUE or FALSE
+ */
+
 public class BitwiseAndOr {
 
 	public static void main(String[] args) {
@@ -12,14 +18,16 @@ public class BitwiseAndOr {
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the values for truth table(0 or 1)");
-		for (int i = 0; i < 4; i++) 
+		// accepting the input as matrix as it can be easily used to display in table
+		for (int i = 0; i < 4; i++)  // to display the 4 rows in the truth table
         {
-            for (int j = 0; j < 2; j++) 
+            for (int j = 0; j < 2; j++) // to display the 2 columns in the truth table
             {
             	a[i][j] = input.nextByte();
             }
         }
 		System.out.println("Truth Table");
+		// displaying the two columns of truth table
 		System.out.println("A \t \t B");
 		for (int i = 0; i < 4; i++) 
         {
@@ -34,12 +42,13 @@ public class BitwiseAndOr {
 		{
 			for(int j = 0; j < 2; j++)
 			{
-				and[i][j] = (byte) (a[i][j] & a[i][++j]);
-				or[i][j] = (byte) (a[i][--j] | a[i][++j]);
+				and[i][j] = (byte) (a[i][j] & a[i][++j]); // bitwise and of the column values of the matrix
+				or[i][j] = (byte) (a[i][--j] | a[i][++j]);// bitwise or of the column values of the matrix
 
 			}
 			
 		}
+		//Displaying the output of Bitwise And
 		System.out.println("Bitwise AND");
 		for(int i = 0; i < 4; i++)
 		{
@@ -47,6 +56,7 @@ public class BitwiseAndOr {
 			System.out.println(and[i][0]);
 			
 		}
+		//Displaying the output of Bitwise Or
 		System.out.println("Bitwise OR");
 		for(int i = 0; i < 4; i++)
 		{
